aad48de3d7bace0c8f8dd92dc2a7446a2579c79d
Dylan Ngo <it.tinhngo@gmail.com>
2022-01-05 23:21:34 +0700

    add sqs long polling

---
+++ src/main/java/com/dylanngo/starter/LongPolling.java
@@ -0,0 +1,71 @@
+package com.dylanngo.starter;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class LongPolling {
+
+  private String QUEUE_NAME = "dylan-sqs";
+
+  public void polling() {
+
+    while (true) {
+      // Create a SqsClient object
+      SqsClient sqsClient = SqsClient.builder()
+        .region(Region.AP_SOUTHEAST_1)
+        .build();
+      setLongPoll(sqsClient);
+      sqsClient.close();
+    }
+
+  }
+
+  public void setLongPoll(SqsClient sqsClient) {
+
+    // Enable long polling when creating a queue.
+    HashMap<QueueAttributeName, String> attributes = new HashMap<QueueAttributeName, String>();
+    attributes.put(QueueAttributeName.RECEIVE_MESSAGE_WAIT_TIME_SECONDS, "5");
+
+    CreateQueueRequest createRequest = CreateQueueRequest.builder()
+      .queueName(QUEUE_NAME)
+      .attributes(attributes)
+      .build();
+
+    try {
+      GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+        .queueName(QUEUE_NAME)
+        .build();
+      String queueUrl = sqsClient.getQueueUrl(getQueueRequest).queueUrl();
+
+      // Enable long polling on an existing queue.
+      SetQueueAttributesRequest setAttrsRequest = SetQueueAttributesRequest.builder()
+        .queueUrl(queueUrl)
+        .attributes(attributes)
+        .build();
+
+      sqsClient.setQueueAttributes(setAttrsRequest);
+
+      // Enable long polling on a message receipt.
+      ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+        .queueUrl(queueUrl)
+        .waitTimeSeconds(10)
+        .build();
+
+      sqsClient.receiveMessage(receiveRequest);
+
+      List<Message> messages = sqsClient.receiveMessage(receiveRequest).messages();
+
+      // Print out the messages
+      for (Message m : messages) {
+        System.out.println("\n" +m.body());
+      }
+    } catch (SqsException e) {
+      System.err.println(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+    }
+  }
+}
--- src/main/java/com/dylanngo/starter/MainVerticle.java
+++ src/main/java/com/dylanngo/starter/MainVerticle.java
@@ -7,12 +7,19 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+
     var sender = new Sender();
     var receive = new Receive();
 
+    new Thread(() -> {
+      var longPolling = new LongPolling();
+      longPolling.polling();
+    }).start();
+
     vertx.createHttpServer().requestHandler(req -> {
-//      sender.send();
-      receive.receive();
+      sender.send();
+//      receive.receive();
+
       req.response()
         .putHeader("content-type", "text/plain")
         .end("Hello from Vert.x!");
